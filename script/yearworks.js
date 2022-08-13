d09cdf7a73f29dc6d518e82995087762ee32eca9
hmc1206 <67060344+hmc1206@users.noreply.github.com>
2022-08-13 21:20:17 +0900

    feat

--- script/yearworks.js
+++ script/yearworks.js
@@ -1,3 +1,19 @@
 document.querySelector('.go-back').addEventListener("click", ()=>{
     history.back();
 })
+
+function filter() {
+    let search = document.getElementById("search").value.toLowerCase();
+    let project_box = document.getElementsByClassName("project_box");
+
+    for (let i = 0; i < project_box.length; i++) {
+      title = project_box[i].getElementsByClassName("title");
+      subheading = project_box[i].getElementsByClassName("subheading");
+      if (title[0].innerHTML.toLowerCase().indexOf(search) != -1 || subheading[0].innerHTML.toLowerCase().indexOf(search) != -1){
+        project_box[i].style.display = "flex";
+      } else {
+        console.dir(title);
+        project_box[i].style.display = "none";
+      }
+    }
+  }

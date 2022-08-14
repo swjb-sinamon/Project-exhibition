e614fe349d8f12908250e921450664f1c18feef5
hmc1206 <67060344+hmc1206@users.noreply.github.com>
2022-08-14 19:22:05 +0900

    dosc

--- script/uploadworks.js
+++ script/uploadworks.js
@@ -17,3 +17,34 @@ for(const input of inputs){
         }
     })
 }
+
+const year = document.querySelector('.year');
+const month = document.querySelector('.month');
+const day = document.querySelector('.date');
+
+const date = new Date();
+let yearFirst = date.getFullYear();
+let monthFirst = 1;
+let monthLast = 12;
+
+for(yearFirst; yearFirst >= 2000; yearFirst-=1){
+    const li = document.createElement('li');
+    li.textContent = yearFirst;
+    year.appendChild(li);
+}
+for(monthFirst; monthFirst<=monthLast; monthFirst++){
+    const li = document.createElement('li');
+    li.innerText = monthFirst;
+    month.appendChild(li);
+}
+
+const yearBtn = document.querySelector('.year-btn');
+const monthBtn = document.querySelector('.month-btn');
+
+yearBtn.addEventListener('click', e=> {
+    year.style.display = "block";
+});
+
+monthBtn.addEventListener('click', ()=> {
+    month.style.display = "block";
+})
